0812fd00287eea0e92bf93c3fe2e0ab4cab12959
Shao-Ching Huang <sch@localhost>
2016-10-13 00:07:56 -0700

    update

--- gather.cpp
+++ gather.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 
     /* initial data on each process */
     for (int i=0; i<N/nproc; i++) {
-	data[i] = (double)rank;
+	data[i] = (double)rank*100 + i;
     }
 
     /* gather data from all processes onto rank=0 process */
@@ -46,14 +46,38 @@ int main(int argc, char *argv[])
     /* rank=0 process writes results to disk */
 
     if (rank == 0) {
-        sprintf(fn,"data.gather");
-	FILE *fp = fopen(fn,"w");
 	for (int i=0; i<N; i++) {
-	    fprintf(fp,"data_coll[%d] = %f\n",i,data_coll[i]);
+	    printf("data_coll[%d] = %f\n",i,data_coll[i]);
 	}
-	fclose(fp);
     }
     if (data) delete [] data;
     if (data_coll) delete [] data_coll;
     MPI_Finalize();
 }
+
+/* Sample output
+
+$ mpicxx gather.cpp 
+$ mpirun -n 4 ./a.out
+data_coll[0] = 0.000000
+data_coll[1] = 1.000000
+data_coll[2] = 2.000000
+data_coll[3] = 3.000000
+data_coll[4] = 4.000000
+data_coll[5] = 100.000000
+data_coll[6] = 101.000000
+data_coll[7] = 102.000000
+data_coll[8] = 103.000000
+data_coll[9] = 104.000000
+data_coll[10] = 200.000000
+data_coll[11] = 201.000000
+data_coll[12] = 202.000000
+data_coll[13] = 203.000000
+data_coll[14] = 204.000000
+data_coll[15] = 300.000000
+data_coll[16] = 301.000000
+data_coll[17] = 302.000000
+data_coll[18] = 303.000000
+data_coll[19] = 304.000000
+
+*/
